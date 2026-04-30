c5b68c550c3d194e99ea786c859753318db77d62
Carol <carolinevictoria1224@gmail.com>
2026-04-30 15:47:29 -0300

    Praticas de manipulação de variaveis

---
+++ praticas/app-one.py
@@ -0,0 +1,26 @@
+# ============================================================
+# COLETA E AMOSTRAGEM DE DADOS
+# ============================================================
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+soma = a + b
+print(f"A soma de {a} e {b} é: {soma}")
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+soma = a + b + c
+print(f"A soma de {a}, {b} e {c} é: {soma}")
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+subtracao = a - b
+print(f"A subtração de {a} pelo {b} é: {subtracao}")
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+multiplicacao = a * b
+print(f"A multiplicação de {a} por {b} é: {multiplicacao}")
